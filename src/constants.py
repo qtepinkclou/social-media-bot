3ab77bdc7977aee470eaea10e062657439662b6c
qtepinkclou <60346294+kktnl@users.noreply.github.com>
2022-01-31 00:47:57 +0300

    Properly separate compiling and running of code

---
+++ src/constants.py
@@ -0,0 +1,50 @@
+"""Constant variables needed by the program."""
+
+# Variables for: handle_discord.py
+
+# Help Comments:
+
+TOOGLE_PYTHON = 'Toggle between Discord chat prompt and Python command prompt.'
+
+SEND_MEDIA = 'Directly acquire media from multiple URLs.'
+
+SAVE_MEDIA = 'Save media from multiple URLs under a keyword.'
+
+DELETE_MEDIA = 'Delete media related with given keyword.'
+
+PRINT_MEDIA = 'Show a list of saved media under given keyword.'
+
+SHOW_MEDIA = 'Acquire saved media under given keyword.'
+
+DETECT_LANDMARK = '''Get the predicted name
+ of the landmark which is in the
+ provided image with the command.'''
+
+# Variables for: captcha_generator.py
+
+CAPTCHA_LENGTH = 5
+CAPTCHA_HEIGHT = 200
+CAPTCHA_WIDTH = 320
+TEMP_CAPTCHA_FOLDER = 'Temporary/_Captcha'
+CAPTCHA_IMAGE_NAME = 'CAPTCHA.png'
+CAPTCHA_FONT_SIZES = [100, ]
+
+
+# Variables for: detect_landmark.py
+
+TEMP_GOOGLE_LENS_FOLDER = 'Temporary/GoogleLens'
+DISCORDAPP_PATTERN = r'discordapp\.com'
+
+
+# Variables for: getmedia.py
+
+TEMP_FOLDER_NAME = 'Temporary'
+PERM_FOLDER_NAME = 'Permanent'
+TEMP_YT_FOLDER = 'Temporary/youtube'
+TEMP_INSTA_FOLDER = 'Temporary/instagram'
+INSTA_SHORTCODE_PATTERN = r'/p/(.+)/'
+INSTA_PATTERN = r'instagram\.com/p/'
+
+# Variables for: pydiscmd.py
+
+FILLER_FILE_NAME = 'FILLER.py'
